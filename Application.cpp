768a4635a08d2ec51e58d0b1c31f6fc539f29c62
Austin Harms <austin@harmsonline.com>
2021-05-13 12:32:05 -0500

    Add vertex buffer

--- Application.cpp
+++ Application.cpp
@@ -18,10 +18,29 @@ int main(void) {
   glfwMakeContextCurrent(window);
   if (glewInit() != GLEW_OK) return -1;
 
+  float block[12] = {
+  //Front Face
+    0, 0, 0,
+    0, 1, 0,
+    1, 1, 0,
+    1, 0, 0
+  };
+
+
+  unsigned int vertBuff;
+  glGenBuffers(1, &vertBuff);
+  glBindBuffer(GL_ARRAY_BUFFER, vertBuff);
+  glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), block, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
