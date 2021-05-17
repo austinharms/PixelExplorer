eb609f8b4dd9bd57b4042b902891dce0368306ef
Austin Harms <austin@harmsonline.com>
2021-05-16 19:27:42 -0500

    Add debug callback

--- src/Application.cpp
+++ src/Application.cpp
@@ -7,6 +7,15 @@
 #include <sstream>
 
 
+void GLAPIENTRY GLErrorCallback(GLenum source, GLenum type, GLuint id,
+                                GLenum severity, GLsizei length,
+                                const GLchar* message, const void* userParam) {
+  fprintf(stderr,
+          "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+          (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type, severity,
+          message);
+}
+
 static unsigned int CompileShader(unsigned int type, const std::string source) {
   unsigned int id = glCreateShader(type);
   const char* src = source.c_str();
@@ -64,6 +73,7 @@ static unsigned int CreateProgram(const std::string path) {
 }
 
 int main(void) {
+
   GLFWwindow* window;
 
   /* Initialize the library */
@@ -79,6 +89,8 @@ int main(void) {
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
   if (glewInit() != GLEW_OK) return -1;
+  glEnable(GL_DEBUG_OUTPUT);
+  glDebugMessageCallback(GLErrorCallback, 0);
 
   float block[12] = {
   //Front Face
@@ -86,6 +98,11 @@ int main(void) {
     0, 1, 0,
     1, 1, 0,
     1, 0, 0
+  };
+
+  unsigned short index[] = {
+     0,3,2,
+     2,1,0
   };
 
   glUseProgram(CreateProgram("./res/shaders/Basic.shader"));
@@ -97,12 +114,19 @@ int main(void) {
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
   glEnableVertexAttribArray(0);
 
+  unsigned int indexBuff;
+  glGenBuffers(1, &indexBuff);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuff);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned short), index,
+               GL_STATIC_DRAW);
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr);
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
