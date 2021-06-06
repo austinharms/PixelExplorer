d3966b9505eff5038318e84d2fad02ebf42b1526
Austin Harms <austin@harmsonline.com>
2021-06-06 11:41:02 -0500

    Fix material not grabbing shader

--- src/engine/Material.h
+++ src/engine/Material.h
@@ -7,30 +7,53 @@
 
 class Material : public virtual RefCounted {
  public:
-  Material() : _color(1.0f), _texture(nullptr), _id(s_nextID++), _shader(nullptr) {}
+  Material()
+      : _color(1.0f), _texture(nullptr), _id(s_nextID++), _shader(nullptr) {}
+
   Material(glm::vec4 color, Texture* texture = nullptr,
            Shader* shader = nullptr)
-      : _color(color), _texture(texture), _id(s_nextID++), _shader(shader) {}
+      : _color(color), _id(s_nextID++), _shader(nullptr), _texture(nullptr) {
+    setShader(shader);
+    setTexture(texture);
+  }
+
   virtual ~Material() {
     if (_texture != nullptr) _texture->drop();
   }
+
   unsigned int getID() const { return _id; }
+
   void setTexture(Texture* t) {
     if (_texture != nullptr) _texture->drop();
-    t->grab();
     _texture = t;
+    if (_texture == nullptr) return;
+    _texture->grab();
   }
+
+  void setShader(Shader* s) {
+    if (_shader != nullptr) _shader->drop();
+    _shader = s;
+    if (_shader == nullptr) return;
+    _shader->grab();
+  }
+
   glm::vec4 getColor() const { return _color; }
+
   void setColor(float r, float g, float b, float a) {
     _color.r = r;
     _color.g = g;
     _color.b = b;
     _color.a = a;
   }
+
   void setColor(glm::vec4 color) { _color = color; }
+
   void bindTexture(unsigned int slot) { _texture->bind(slot); }
+
   bool hasTexture() const { return _texture != nullptr; }
+
   bool hasShader() const { return _shader != nullptr; }
+
   Shader* getShader() const { return _shader; }
 
  private:
