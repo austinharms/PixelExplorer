7a4ec9dd283037a3b182912acfa932a26192be00
Austin Harms <austin@harmsonline.com>
2021-11-22 20:43:55 -0600

    Fix ChunkManager map not adding

--- PixelExplorer/src/chunk/ChunkManager.cpp
+++ PixelExplorer/src/chunk/ChunkManager.cpp
@@ -1,22 +1,15 @@
 #include "ChunkManager.h"
 
-ChunkManager::ChunkManager()
-{
+ChunkManager::ChunkManager() {}
 
+ChunkManager::~ChunkManager() {}
+
+void ChunkManager::UnloadChunks() {
+  
 }
 
-ChunkManager::~ChunkManager()
-{
-
-}
-
-void ChunkManager::UnloadChunks()
-{
-}
-
-void ChunkManager::LoadChunk(const glm::vec<3, int32_t> pos)
-{
-	Chunk newChunk;
-	newChunk.setPosition(pos);
-	_chunks.insert({const glm::vec<3, int32_t>(pos), newChunk});
+void ChunkManager::LoadChunk(const glm::vec<3, int32_t> pos) {
+  Chunk newChunk;
+  newChunk.setPosition(pos);
+  _chunks.emplace(pos, newChunk);
 }
--- PixelExplorer/src/chunk/ChunkManager.h
+++ PixelExplorer/src/chunk/ChunkManager.h
@@ -2,35 +2,23 @@
 #define CHUNKMANAGER_H
 
 #include <unordered_map>
+#include <string>
 
+#include "Chunk.h"
 #include "RefCounted.h"
 #include "glm/vec3.hpp"
-#include "Chunk.h"
 #define GLM_ENABLE_EXPERIMENTAL
 #include "glm/gtx/hash.hpp"
 
-class ChunkManager : RefCounted
-{
-public:
-	ChunkManager();
-	~ChunkManager();
-	void UnloadChunks();
-	void LoadChunk(glm::vec<3, int32_t> pos);
+class ChunkManager : RefCounted {
+ public:
+  ChunkManager();
+  ~ChunkManager();
+  void UnloadChunks();
+  void LoadChunk(glm::vec<3, int32_t> pos);
 
-private:
-	struct KeyTraits {
-		size_t operator()(const glm::vec<3, int32_t>& k) const {
-			return std::hash<int32_t>()(k.x) ^ std::hash<int32_t>()(k.y);
-		}
-
-		bool operator()(const glm::vec<3, int32_t>& a, const glm::vec<3, int32_t>& b) const {
-			return a == b;
-		}
-	};
-
-	typedef std::unordered_map<glm::vec<3, int32_t>, Chunk, KeyTraits, KeyTraits> ChunkMap;
-	ChunkMap _chunks;
-
+ private:
+  std::unordered_map<glm::vec<3, int32_t>, Chunk> _chunks;
 };
 
 #endif
