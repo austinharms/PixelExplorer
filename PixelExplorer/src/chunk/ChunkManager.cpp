4f6a18cf05da579e4aac410ecec352b6b032f4ae
Austin Harms <austin@harmsonline.com>
2021-12-06 12:43:22 -0600

    Add Adj Chunk Update on Chunk Load

--- PixelExplorer/src/chunk/ChunkManager.cpp
+++ PixelExplorer/src/chunk/ChunkManager.cpp
@@ -3,41 +3,40 @@
 #include <iostream>
 
 ChunkManager::ChunkManager(Renderer* renderer) {
-	_renderer = renderer;
-	renderer->grab();
+  _renderer = renderer;
+  renderer->grab();
 }
 
 ChunkManager::~ChunkManager() {
-	UnloadChunks();
-	_renderer->drop();
+  UnloadChunks();
+  _renderer->drop();
 }
 
 void ChunkManager::UnloadChunks() {
-	for (std::pair<const glm::vec<3, int32_t>, Chunk*>& pair : _chunks) {
-		pair.second->setStatus(Chunk::Status::UNLOADED);
-		pair.second->setRendererDropFlag(true);
-		pair.second->drop();
-	}
+  for (std::pair<const glm::vec<3, int32_t>, Chunk*>& pair : _chunks) {
+    pair.second->setStatus(Chunk::Status::UNLOADED);
+    pair.second->setRendererDropFlag(true);
+    pair.second->drop();
+  }
 
-	_chunks.clear();
+  _chunks.clear();
 }
 
 void ChunkManager::LoadChunk(const glm::vec<3, int32_t> pos) {
-	Chunk* newChunk = new Chunk();
-	_chunks.insert({ pos, newChunk });
-	_renderer->addRenderable(newChunk);
-	newChunk->setPosition(pos);
-	newChunk->setStatus(Chunk::Status::LOADING);
-	newChunk->updateMesh();
-	newChunk->setStatus(Chunk::Status::LOADED);
+  Chunk* newChunk = new Chunk();
+  _chunks.insert({pos, newChunk});
+  _renderer->addRenderable(newChunk);
+  newChunk->setStatus(Chunk::Status::LOADING);
+  newChunk->setPosition(pos);
+  newChunk->setStatus(Chunk::Status::LOADED);
+  newChunk->updateAdjacentChunks(this);
+  newChunk->updateMesh();
 }
 
-Chunk* ChunkManager::GetChunk(glm::vec<3, int32_t> pos)
-{
-	try {
-		return _chunks.at(pos);
-	}
-	catch(...) {
-		return nullptr;
-	}
+Chunk* ChunkManager::GetChunk(glm::vec<3, int32_t> pos) {
+  try {
+    return _chunks.at(pos);
+  } catch (...) {
+    return nullptr;
+  }
 }
