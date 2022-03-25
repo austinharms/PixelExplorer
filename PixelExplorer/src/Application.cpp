3c3cbf27df078a7d4abb16f1bf4e16f2ab6285c9
Austin Harms <austin@harmsonline.com>
2022-03-25 10:17:57 -0500

    Remove old world and application code

--- PixelExplorer/src/Application.cpp
+++ PixelExplorer/src/Application.cpp
@@ -2,80 +2,97 @@
 //#include <crtdbg.h>
 //#include <stdlib.h>
 
-#include <fstream>
-#include <iostream>
-#include <thread>
-
-#include "GL/glew.h"
-#include "GLFW/glfw3.h"
-#include "World.h"
-#include "chunk/Chunk.h"
-#include "chunk/block/BaseBlock.h"
 #include "rendering/Renderer.h"
-#include "rendering/Shader.h"
-#include "rendering/TestRenderable.h"
-#include "physics/PhysicsManager.h"
+#include "physics/PhysicsBase.h"
+#include "Game.h"
 
+int main(void) { 
+    Renderer* renderer = new Renderer(1200, 800, "Pixel Explorer V2.0", 60);
+    renderer->setCursorHidden(true);
+    PhysicsBase* physics = PhysicsBase::CreatePhysicsBase();
 
-int main(void) {
-  PhysicsManager::Init();
-  Renderer* renderer = new Renderer(1200, 800, "Pixel Explorer V2.0", 60);
-  renderer->setCursorHidden(true);
-  World::SetRenderer(renderer);
-  World::LoadWorld();
-  for (uint8_t x = 0; x < 10; ++x)
-    for (uint8_t y = 0; y < 10; ++y)
-      for (uint8_t z = 0; z < 10; ++z)
-        World::GetChunkManager()->loadChunk(glm::vec<3, int32_t>(x, y, z));
-  World::GetChunkManager()->UpdateLoadedChunks();
-  //World::GetChunkManager()->LoadChunk(glm::vec<3, int32_t>(0,0,0));
-  glm::vec3 camPos(12, 12, 75);
-  glm::vec3 camRot(0, 0, 0);
-  float moveSpeed = 50;
-  while (renderer->getWindowOpen()) {
-    if (renderer->getKeyPressed(GLFW_KEY_W)) {
-      camPos += renderer->getDeltaTime() * renderer->getForward() * moveSpeed;
-    } else if (renderer->getKeyPressed(GLFW_KEY_S)) {
-      camPos -= renderer->getDeltaTime() * renderer->getForward() * moveSpeed;
-    }
+    Game* game = new Game(renderer, physics);
 
-    if (renderer->getKeyPressed(GLFW_KEY_A)) {
-      camPos -= renderer->getDeltaTime() *
-                glm::cross(renderer->getForward(), glm::vec3(0, 1, 0)) *
-                moveSpeed;
-    } else if (renderer->getKeyPressed(GLFW_KEY_D)) {
-      camPos += renderer->getDeltaTime() *
-                glm::cross(renderer->getForward(), glm::vec3(0, 1, 0)) *
-                moveSpeed;
-    }
-
-    if (renderer->getKeyPressed(GLFW_KEY_SPACE)) {
-      camPos.y += renderer->getDeltaTime() * moveSpeed;
-    } else if (renderer->getKeyPressed(GLFW_KEY_LEFT_CONTROL)) {
-      camPos.y -= renderer->getDeltaTime() * moveSpeed;
-    }
-
-    camRot.x += 0.001f * renderer->getCursorChangeY();
-    camRot.y += 0.001f * renderer->getCursorChangeX();
-
-    if (renderer->getKeyPressed(GLFW_KEY_ESCAPE))
-      renderer->setCursorHidden(false);
-
-    renderer->setTransform(camPos, camRot);
-    renderer->drawFrame();
-    World::AddSimulationTime(renderer->getDeltaTime());
-  }
-
-  // ORDER IMPORTANT
-  World::UnloadWorld();
-  World::SetRenderer(nullptr);
-  Material::getDefault()->drop();
-  Shader::getDefault()->drop();
-  renderer->drop();
-  // END OF ORDER IMPORTANT
-  PhysicsManager::Destroy();
-  Chunk::FreeStaticMembers();
-  glfwTerminate();
-  //_CrtDumpMemoryLeaks();
+    game->drop();
+    physics->drop();
+    renderer->drop();
   return 0;
 }
+
+//#include <fstream>
+//#include <iostream>
+//#include <thread>
+//
+//#include "GL/glew.h"
+//#include "GLFW/glfw3.h"
+//#include "World.h"
+//#include "chunk/Chunk.h"
+//#include "chunk/block/BlockDefinition.h"
+//#include "physics/PhysicsManager.h"
+//#include "rendering/Renderer.h"
+//#include "rendering/Shader.h"
+//#include "rendering/TestRenderable.h"
+
+//int main(void) {
+//  BlockDefinition::LoadDefinitions();
+//  PhysicsManager::Init();
+//  Renderer* renderer = new Renderer(1200, 800, "Pixel Explorer V2.0", 60);
+//  renderer->setCursorHidden(true);
+//  World::SetRenderer(renderer);
+//  World::LoadWorld();
+//  for (uint8_t x = 0; x < 10; ++x)
+//    for (uint8_t y = 0; y < 10; ++y)
+//      for (uint8_t z = 0; z < 10; ++z)
+//        World::GetChunkManager()->loadChunk(glm::vec<3, int32_t>(x, y, z));
+//  World::GetChunkManager()->UpdateLoadedChunks();
+//  // World::GetChunkManager()->LoadChunk(glm::vec<3, int32_t>(0,0,0));
+//  glm::vec3 camPos(12, 12, 75);
+//  glm::vec3 camRot(0, 0, 0);
+//  float moveSpeed = 50;
+//  while (renderer->getWindowOpen()) {
+//    if (renderer->getKeyPressed(GLFW_KEY_W)) {
+//      camPos += renderer->getDeltaTime() * renderer->getForward() * moveSpeed;
+//    } else if (renderer->getKeyPressed(GLFW_KEY_S)) {
+//      camPos -= renderer->getDeltaTime() * renderer->getForward() * moveSpeed;
+//    }
+//
+//    if (renderer->getKeyPressed(GLFW_KEY_A)) {
+//      camPos -= renderer->getDeltaTime() *
+//                glm::cross(renderer->getForward(), glm::vec3(0, 1, 0)) *
+//                moveSpeed;
+//    } else if (renderer->getKeyPressed(GLFW_KEY_D)) {
+//      camPos += renderer->getDeltaTime() *
+//                glm::cross(renderer->getForward(), glm::vec3(0, 1, 0)) *
+//                moveSpeed;
+//    }
+//
+//    if (renderer->getKeyPressed(GLFW_KEY_SPACE)) {
+//      camPos.y += renderer->getDeltaTime() * moveSpeed;
+//    } else if (renderer->getKeyPressed(GLFW_KEY_LEFT_CONTROL)) {
+//      camPos.y -= renderer->getDeltaTime() * moveSpeed;
+//    }
+//
+//    camRot.x += 0.001f * renderer->getCursorChangeY();
+//    camRot.y += 0.001f * renderer->getCursorChangeX();
+//
+//    if (renderer->getKeyPressed(GLFW_KEY_ESCAPE))
+//      renderer->setCursorHidden(false);
+//
+//    renderer->setTransform(camPos, camRot);
+//    renderer->drawFrame();
+//    World::AddSimulationTime(renderer->getDeltaTime());
+//  }
+//
+//  // ORDER IMPORTANT
+//  World::UnloadWorld();
+//  World::SetRenderer(nullptr);
+//  Material::getDefault()->drop();
+//  Shader::getDefault()->drop();
+//  renderer->drop();
+//  // END OF ORDER IMPORTANT
+//  PhysicsManager::Destroy();
+//  BlockDefinition::UnloadDefinitions();
+//  glfwTerminate();
+//  //_CrtDumpMemoryLeaks();
+//  return 0;
+//}
--- PixelExplorer/src/World.h
+++ PixelExplorer/src/World.h
@@ -1,64 +1,15 @@
 #ifndef WORLD_H
 #define WORLD_H
-#include <string>
-#include <thread>
-#include <vector>
 
-#include "package/Package.h"
-#include "package/PackageLoader.h"
-#include "chunk/ChunkManager.h"
-#include "rendering/Renderer.h"
+#include "RefCounted.h"
 
-class World {
+class World : public RefCounted {
  public:
-  static const uint16_t MANIFEST_VERSION = 1;
+  static World* LoadWorld();
 
-  // Should ONLY be called on the MAIN thread
-  static void LoadWorld();
-  // Should ONLY be called on the MAIN thread
-  static void UnloadWorld();
-
-  static void SetPhysicsPaused(bool paused) { s_physXPause = paused; }
-
-  static bool GetPhysicsPaused() { return s_physXPause; }
-
-  static const std::string GetWorldDir() { return s_worldDir; }
-
-  static const std::vector<Package*> GetPackages() { return s_packages; }
-
-  static const Package* GetPackage(std::string name) {
-    std::vector<Package*>::iterator it;
-    for (it = s_packages.begin(); it != s_packages.end(); ++it)
-      if ((*it)->GetName() == name) return *it;
-    return nullptr;
-  }
-
-  static ChunkManager* GetChunkManager() { return s_chunkManager; }
-
-  static void SetRenderer(Renderer* renderer) {
-    if (s_renderer != nullptr) s_renderer->drop();
-    s_renderer = renderer;
-    if (s_renderer != nullptr) s_renderer->grab();
-  }
-
-  static void AddSimulationTime(float time) {
-    if (!s_physXPause && s_physXActive)
-      s_physXAccumulator = s_physXAccumulator + time;
-  }
+  virtual ~World();
 
  private:
-  World() {}
-  ~World() {}
-  static void updateManifest();
-  static void physXUpdateLoop();
-  static std::string s_worldDir;
-  static volatile bool s_physXActive;
-  static volatile bool s_physXPause;
-  static std::vector<Package*> s_packages;
-  static ChunkManager* s_chunkManager;
-  static Renderer* s_renderer;
-  static std::thread* s_physXThread;
-  static std::atomic<float> s_physXAccumulator;
-  static const float s_physXTimeStep;
+  World();
 };
 #endif
