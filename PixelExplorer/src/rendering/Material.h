2e0b4b8583c21dd1a8f04625d65850e99434b627
Austin Harms <austin@harmsonline.com>
2022-05-31 14:40:24 -0500

    Update Material properties

--- PixelExplorer/src/rendering/Material.h
+++ PixelExplorer/src/rendering/Material.h
@@ -4,6 +4,10 @@
 
 #include "RefCount.h"
 #include "Shader.h"
+#include "glm/vec2.hpp"
+#include "glm/vec3.hpp"
+#include "glm/vec4.hpp"
+#include "glm/mat4x4.hpp"
 
 #ifndef PIXELEXPLORE_RENDERING_MATERIAL_H_
 #define PIXELEXPLORE_RENDERING_MATERIAL_H_
@@ -25,7 +29,24 @@ namespace pixelexplore::rendering {
 		virtual ~Material();
 		virtual void applyMaterial(Shader* shader);
 		virtual void removeProperty(const std::string& propertyName);
-		virtual void addProperty(const std::string& propertyName);
+		void addProperty(const std::string& propertyName, float value);
+		void addProperty(const std::string& propertyName, int32_t value);
+		void addProperty(const std::string& propertyName, glm::vec2 value);
+		void addProperty(const std::string& propertyName, glm::vec3 value);
+		void addProperty(const std::string& propertyName, glm::vec4 value);
+		void addProperty(const std::string& propertyName, glm::mat4 value);
+
+	protected:
+		struct MaterialProperty
+		{
+			uint32_t typeId;
+			void* valuePtr;
+			~MaterialProperty() {
+				free(valuePtr);
+			}
+		};
+
+		virtual void applyProperty(Shader* shader, MaterialProperty p);
 	private:
 
 	};
