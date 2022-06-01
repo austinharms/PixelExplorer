b578e9c7994c6feaf705597834f7374db4d3f471
Austin Harms <austin@harmsonline.com>
2022-06-01 10:52:25 -0500

    Add Memory Leak Checks to Main

--- PixelExplorer/src/main.cpp
+++ PixelExplorer/src/main.cpp
@@ -1,9 +1,15 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 #include <stdint.h>
 #include "Logger.h"
 #include "rendering/RenderWindow.h"
 #include "rendering/RenderMesh.h"
 
 int main(void) {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	pixelexplore::rendering::RenderWindow* window = new pixelexplore::rendering::RenderWindow(600, 400, "Pixel Explore");
 	pixelexplore::rendering::RenderMesh* renderMesh = new pixelexplore::rendering::RenderMesh();
 
