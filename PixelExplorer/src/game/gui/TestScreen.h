ac7ee33526769169557fc94b0e8d186bc6514608
Austin Harms <austin@harmsonline.com>
2022-06-03 18:12:15 -0500

    Add Texture

--- PixelExplorer/src/game/gui/TestScreen.h
+++ PixelExplorer/src/game/gui/TestScreen.h
@@ -1,8 +1,11 @@
 #include <stdint.h>
 #include <string>
 
+#include "Logger.h"
 #include "imgui.h"
+#include "rendering/RenderWindow.h"
 #include "rendering/GUIElement.h"
+#include "rendering/Texture.h"
 
 #ifndef PIXELEXPLORE_GAME_GUI_TESTSCREEN_H_
 #define PIXELEXPLORE_GAME_GUI_TESTSCREEN_H_
@@ -25,13 +28,17 @@ namespace pixelexplorer::game::gui {
 			_windowFlags |= ImGuiWindowFlags_UnsavedDocument;
 			//_windowFlags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
 			//_windowFlags |= ImGuiWindowFlags_MenuBar;
+			_texture = new rendering::Texture("./assets/textures/testbackground.png");
 		}
 
-		inline virtual ~TestScreen() {}
+		inline virtual ~TestScreen() { _texture->drop(); }
+
 		inline void drawElement(float windowWidth, float windowHeight) {
 			ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
 			ImGui::SetNextWindowSize(ImVec2(windowWidth, windowHeight), ImGuiCond_Always);
 			ImGui::Begin("Main Menu", nullptr, _windowFlags);
+			ImGui::SetCursorPos(ImVec2(0, 0));
+			ImGui::Image((ImTextureID)_texture->getGlId(), ImVec2(windowWidth, windowHeight));
 			ImGui::SetCursorPos(ImVec2(windowWidth / 2 - 25, windowHeight / 2 - 10));
 			if (ImGui::Button("Click", ImVec2(50, 20)))
 				++_clickCounter;
@@ -45,8 +52,30 @@ namespace pixelexplorer::game::gui {
 
 		inline bool getShouldClose() const { return _shouldClose; }
 		inline bool getRemoveTestMesh() const { return _removeTestMesh; }
+		inline bool requiresGLObjects() { return true; }
+
+		inline void createGLObjects(rendering::RenderWindow* window) {
+			if (getHasGLObjects()) {
+				Logger::warn(__FUNCTION__ " Attempted to overwrite GL objects");
+				return;
+			}
+
+			_texture->createGlTexture();
+			setHasGLObjects(true);
+		}
+
+		inline void destroyGLObjects(rendering::RenderWindow* window) {
+			if (!getHasGLObjects()) {
+				Logger::warn(__FUNCTION__ " Attempted to delete empty GL objects");
+				return;
+			}
+
+			_texture->deleteGlTexture();
+			setHasGLObjects(false);
+		}
 
 	private:
+		rendering::Texture* _texture;
 		uint32_t _clickCounter;
 		ImGuiWindowFlags _windowFlags;
 		bool _shouldClose;
--- PixelExplorer/src/rendering/Texture.h
+++ PixelExplorer/src/rendering/Texture.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <string>
 
 #include "RefCount.h"
 #include "stb_image.h"
@@ -9,11 +10,21 @@ namespace pixelexplorer::rendering {
 	class Texture : public RefCount
 	{
 	public:
-		Texture();
+		Texture(uint32_t width, uint32_t height);
+		Texture(const std::string& path);
 		virtual ~Texture();
+		void createGlTexture();
+		void deleteGlTexture();
+		void bind(uint8_t slot = 0);
+		inline uint32_t getGlId() const { return _glTextureId; }
 
 	private:
 		uint8_t* _pixelBuffer;
+		uint32_t _glTextureId;
+		uint32_t _width;
+		uint32_t _height;
+		bool _stbImage;
+		bool _hasGlObject;
 	};
 }
 #endif // !PIXELEXPLORER_RENDERING_TEXTURE_H_
