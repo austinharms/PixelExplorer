e0ec9f1a0c7a3745184e5f6aafe217f3a282a495
Austin Harms <austin@harmsonline.com>
2022-11-28 20:13:14 -0600

    Start BufferFormats

---
+++ PXEngine/include/PxeGlVertexBufferFormat.h
@@ -0,0 +1,12 @@
+#ifndef PXENGINE_GLVERTEXTBUFFERFORMAT_H_
+#define PXENGINE_GLVERTEXTBUFFERFORMAT_H_
+class PxeGlVertexBufferFormat
+{
+public:
+	PxeGlVertexBufferFormat();
+	~PxeGlVertexBufferFormat();
+
+private:
+
+};
+#endif // !PXENGINE_GLVERTEXTBUFFERFORMAT_H_
