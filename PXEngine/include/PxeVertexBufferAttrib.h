f570e3618063e72208c1175aeae3436f510e8600
Austin Harms <austin@harmsonline.com>
2022-11-30 21:27:09 -0600

    Continue VertexBufferAttrib

--- PXEngine/include/PxeVertexBufferAttrib.h
+++ PXEngine/include/PxeVertexBufferAttrib.h
@@ -1,14 +1,16 @@
+#include <stdint.h>
+
+#include "gl/GL.h"
+
 #ifndef PXENGINE_VERTEXBUFFERATTRIB_H_
 #define PXENGINE_VERTEXBUFFERATTRIB_H_
 namespace pxengine {
-	class PxeVertexBufferAttrib
+	struct PxeVertexBufferAttrib
 	{
 	public:
-		PxeVertexBufferAttrib();
-		~PxeVertexBufferAttrib();
-
-	private:
-
+		uint32_t ComponentType;
+		uint8_t ComponentCount;
+		bool Normalized;
 	};
 }
 #endif
