ddeee5650f2c828a61cc4f3bc8d936dfcd690e88
Austin Harms <austin@harmsonline.com>
2023-03-22 17:08:37 -0500

    Add text color PX log class

--- PixelExplorer/src/Log.cpp
+++ PixelExplorer/src/Log.cpp
@@ -1,9 +1,45 @@
 #include "Log.h"
 
-#include "PxeEngine.h"
 #include <iostream>
 
+#include "PxeEngine.h"
+
+/*
+	Color Table
+	00: Black
+	01: Blue
+	02: Green
+	03: Aqua
+	04: Red
+	05: Purple
+	06: Yellow
+	07: White
+	08: Gray
+	09: Light_Blue
+	10: Light_Green
+	11: Light_Aqua
+	12: Light_Red
+	13: Light_Purple
+	14: Light_Yellow
+	15: Bright_White
+*/
+
 namespace pixelexplorer {
+	Log::Log()
+	{
+#ifdef PXE_WIN_OS
+		_stdOutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleTextAttribute(_stdOutHandle, 7);
+#endif // PXE_WIN_OS
+	}
+
+	Log::~Log()
+	{
+#ifdef PXE_WIN_OS
+		SetConsoleTextAttribute(_stdOutHandle, 7);
+#endif // PXE_WIN_OS
+	}
+
 	void Log::log(pxengine::PxeLogLevel level, const char* msg, const char* file, const char* function, uint64_t line)
 	{
 		pxengine::PxeEngine::getInstance().getLoggingInterface().onLog(level, msg, file, function, line);
@@ -14,15 +50,27 @@ namespace pixelexplorer {
 		switch (level)
 		{
 		case pxengine::PxeLogLevel::PXE_INFO:
+#ifdef PXE_WIN_OS
+			SetConsoleTextAttribute(_stdOutHandle, 7);
+#endif // PXE_WIN_OS
 			std::cout << "[INFO] ";
 			break;
 		case pxengine::PxeLogLevel::PXE_WARN:
+#ifdef PXE_WIN_OS
+			SetConsoleTextAttribute(_stdOutHandle, 14);
+#endif // PXE_WIN_OS
 			std::cout << "[WARN] ";
 			break;
 		case pxengine::PxeLogLevel::PXE_ERROR:
+#ifdef PXE_WIN_OS
+			SetConsoleTextAttribute(_stdOutHandle, 12);
+#endif // PXE_WIN_OS
 			std::cout << "[ERROR] ";
 			break;
 		case pxengine::PxeLogLevel::PXE_FATAL:
+#ifdef PXE_WIN_OS
+			SetConsoleTextAttribute(_stdOutHandle, 4);
+#endif // PXE_WIN_OS
 			std::cout << "[FATAL] ";
 			break;
 		}
--- PixelExplorer/src/Log.h
+++ PixelExplorer/src/Log.h
@@ -2,6 +2,11 @@
 #define PIXELEXPLORER_LOG_H_
 #include "PxeTypes.h"
 #include "PxeLogger.h"
+#include "PxeOSHelpers.h"
+
+#ifdef PXE_WIN_OS
+#include <Windows.h>
+#endif // PXE_OS_WIN
 
 #define PEX_INFO(msg) pixelexplorer::Log::log(pxengine::PxeLogLevel::PXE_INFO, msg, __FILE__, __FUNCTION__, __LINE__);
 #define PEX_WARN(msg) pixelexplorer::Log::log(pxengine::PxeLogLevel::PXE_WARN, msg, __FILE__, __FUNCTION__, __LINE__);
@@ -14,6 +19,14 @@ namespace pixelexplorer {
 	public:
 		static void log(pxengine::PxeLogLevel level, const char* msg, const char* file, const char* function, uint64_t line);
 		void onLog(pxengine::PxeLogLevel level, const char* msg, const char* file, const char* function, uint64_t line) override;
+		Log();
+		~Log();
+
+	private:
+#ifdef PXE_WIN_OS
+		HANDLE _stdOutHandle;
+#endif // PXE_WIN_OS
+
 	};
 }
 #endif // !PIXELEXPLORER_LOG_H_
