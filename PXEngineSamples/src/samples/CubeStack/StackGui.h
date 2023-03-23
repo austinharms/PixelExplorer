273141a229857a49737fa59f9cda338a7bf5e18f
Austin Harms <austin@harmsonline.com>
2023-03-23 13:11:17 -0500

    Update cube stack sample gui

--- PXEngineSamples/src/samples/CubeStack/StackGui.h
+++ PXEngineSamples/src/samples/CubeStack/StackGui.h
@@ -40,14 +40,16 @@ protected:
 		ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
 		ImGuiIO& io = ImGui::GetIO();
-		ImGui::Begin("Cube Stack Gui", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoBringToFrontOnFocus);
+		ImGui::Begin("Cube Stack Gui", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoBackground);
 		ImGui::Text("FPS: %.0f", io.Framerate);
 		ImGui::Text("Controls");
+		ImGui::PushItemWidth(75);
 		ImGui::DragInt("Cube Count", &_state.CubeCount, 1, 1);
 		ImGui::DragFloat("Cube Spacing", &_state.CubeSpacing, 0.05f);
 		ImGui::DragFloat("Base Height", &_state.BaseHeight, 0.25f);
 		ImGui::DragFloat("Time Scale", &_state.TimeScale, 0.01f, 0, 100);
 		ImGui::DragFloat("Physics Step", &_state.PhysicsStep, 0.001f, 0.001, 1);
+		ImGui::PopItemWidth();
 		if (ImGui::Button("Reset Stack"))
 			_state.Reset = true;
 		ImGui::End();
