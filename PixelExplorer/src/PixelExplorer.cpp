694ce49ead03730d8a851ec4de3f3f84ac07a084
Austin Harms <austin@harmsonline.com>
2024-02-19 14:29:12 -0600

    Add project files.

---
+++ PixelExplorer/src/PixelExplorer.cpp
@@ -0,0 +1,12 @@
+﻿// PixelExplorer.cpp : Defines the entry point for the application.
+//
+
+#include "PixelExplorer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
